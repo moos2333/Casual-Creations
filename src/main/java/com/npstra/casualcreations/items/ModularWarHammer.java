67fd43b2fe0783461bfaef12e1202d31006cff54
moos2333 <xvruikai0804@126.com>
2026-07-22 11:27:52 +0800

    Update ModularWarHammer.java

--- src/main/java/com/npstra/casualcreations/items/ModularWarHammer.java
+++ src/main/java/com/npstra/casualcreations/items/ModularWarHammer.java
@@ -63,7 +63,7 @@ public class ModularWarHammer extends ItemPickaxe implements IModularTool {
     public float getDestroySpeed(ItemStack stack, IBlockState state) {
         float original = super.getDestroySpeed(stack, state);
         if (original <= 1.0f) return original;
-        return ModularToolHelper.getDestroySpeed(stack, 0.5f) * 0.9f;
+        return ModularToolHelper.getDestroySpeed(stack, 0.5f) * 0.8f;
     }
 
     @Override
